7229cb2df4bf1810460ea4bb62caadd383cf8163
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2022-10-13 21:55:19 +0900

    [MAIN] 반복되는 컴포넌트에 map 사용하기 & props를 통해 부모에서 자식으로 값 넘기기

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,12 @@ function App() {
     "유후유후",
     "가나초코렡",
   ]);
-  let [like, setLike] = useState(0);
+  let [isModalOpen, setIsModalOpen] = useState(false);
+  let [selectedContentIndex, setSelectedContentIndex] = useState<number | null>(
+    null
+  );
 
-  function onClickContent() {
+  function onClickEditContent() {
     // let copy = [...contentList];
     // copy[0] = "나는 최윤정, who is hungry";
     // setContentList(copy);
@@ -23,10 +26,6 @@ function App() {
     });
   }
 
-  function onClickLike() {
-    setLike(like + 1);
-  }
-
   function onClickSort() {
     setContentList((prev) => {
       const copy = [...prev];
@@ -36,27 +35,66 @@ function App() {
     });
   }
 
+  function onClickContent(index: number) {
+    setIsModalOpen(!isModalOpen);
+    setSelectedContentIndex(index);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <h4 id={title}>{title}</h4>
       </div>
       <button onClick={onClickSort}>가나다순정렬</button>
-      <div className="item">
+      {/* <div className="item">
         <h4 style={{ color: "crimson" }} onClick={onClickContent}>
           {contentList[0]}
           <span onClick={onClickLike}>👍{like}</span>
         </h4>
         <p>{`${new Date()} 발행`}</p>
-      </div>
-      <div className="item">
-        <h4 style={{ color: "crimson" }}>{contentList[1]}</h4>
-        <p>{`${new Date()} 발행`}</p>
-      </div>
-      <div className="item">
-        <h4 style={{ color: "crimson" }}>{contentList[2]}</h4>
-        <p>{`${new Date()} 발행`}</p>
-      </div>
+      </div> */}
+      {contentList.map((content, index) => (
+        <div key={content} className="item">
+          <h4
+            style={{ color: "crimson" }}
+            onClick={() => onClickContent(index)}
+          >
+            {content}
+          </h4>
+          <Like />
+          <p>{`${new Date()} 발행`}</p>
+        </div>
+      ))}
+      {isModalOpen && (
+        <Modal
+          content={
+            selectedContentIndex !== null
+              ? contentList[selectedContentIndex]
+              : undefined
+          }
+          onClickEditContent={onClickEditContent}
+        />
+      )}
+    </div>
+  );
+}
+
+function Like() {
+  let [like, setLike] = useState(0);
+
+  function onClickLike() {
+    setLike(like + 1);
+  }
+  return <span onClick={onClickLike}>👍{like}</span>;
+}
+
+function Modal(props: { content?: string; onClickEditContent: () => void }) {
+  return (
+    <div className="modal">
+      <h4>{props.content}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={props.onClickEditContent}>글수정</button>
     </div>
   );
 }
